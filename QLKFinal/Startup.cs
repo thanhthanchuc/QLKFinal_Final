0644b91fd8603ef4366008440971f6a6fbda9d23
Nguyen Thanh <Nguyen Thanh@NGUYENTHANH-PC>
2018-05-02 13:38:32 +0700

    Add project files.

---
+++ QLKFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QLKFinal.Startup))]
+namespace QLKFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
