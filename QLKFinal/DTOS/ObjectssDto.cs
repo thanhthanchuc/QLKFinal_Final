f16fb3b6001ef77efeddb5f8b7737f65d4c88de2
Nguyen Thanh <Nguyen Thanh@NGUYENTHANH-PC>
2018-05-03 21:12:39 +0700

    Data Transfer Objects (Dto)

---
+++ QLKFinal/DTOS/ObjectssDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using QLKFinal.Models;
+
+namespace QLKFinal.DTOS
+{
+    public class ObjectssDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string DisplayName { get; set; }
+
+        [Required]
+        public int? Count { get; set; }
+
+        [Required]
+        public DateTime? DateAdded { get; set; }
+
+        public Suplier Suplier { get; set; }
+
+
+        public Unit Unit { get; set; }
+
+    }
+}
---
+++ QLKFinal/DTOS/SuplierDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QLKFinal.DTOS
+{
+    public class SuplierDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Bạn không được bỏ chống trường này!")]
+        public string DisplayName { get; set; }
+
+
+        public string Addresss { get; set; }
+
+        [Required(ErrorMessage = "Bạn không được bỏ chống trường này!")]
+        public string Email { get; set; }
+
+        [Phone]
+        public int PhoneNumber { get; set; }
+
+        public string MoreInfo { get; set; }
+
+        [Required(ErrorMessage = "Bạn không được bỏ chống trường này!")]
+        public DateTime? ContractDate { get; set; }
+    }
+}
